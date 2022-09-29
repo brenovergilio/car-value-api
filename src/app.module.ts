fba8d464c89a20940aa40f2aec9e16f318897c5b
Breno Vergilio <bjvergilio47@gmail.com>
2022-09-29 10:44:30 -0400

    feat: generate users and reports module, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { ReportosService } from './reportos/reportos.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReportosService],
 })
 export class AppModule {}
